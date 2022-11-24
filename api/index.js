e155e8e9efc7896742d3c3a2f92063fa5375e140
innocent98 <tosinadebayo55@gmail.com>
2022-11-24 08:30:26 +0000

    updated

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth");
+const router = require("./routes/index");
 const multer = require("multer");
 const path = require("path");
 
@@ -14,20 +14,21 @@ dotenv.config();
 // database connection
 mongoose
   .connect(process.env.MONGO_URL, {
-    useUnifiedTopology: true,
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .then(console.log("database connected"))
+  .then(() => console.log("database connected!"))
   .catch((err) => console.log(err));
-  
-  // get images
-  app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// get images
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //   middlewares
-app.use(morgan("common"));
-app.use(cors());
-app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("common"));
+app.use(helmet());
 
 // flie upload
 const storage = multer.diskStorage({
@@ -48,8 +49,8 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   }
 });
 
-// router
-app.use("/api/auth", authRoute);
+// user router
+app.use(router);
 
 app.listen(process.env.PORT || 8300, () => {
   console.log("server listening to port 8300");
